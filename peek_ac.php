6c52a7351068624f0c2a0e72b6b868582aae43b3
agadape <agadape@gmail.com>
2026-09-22 09:32:44 +0700

    chore: stop tracking generated artifacts

--- peek_ac.php
+++
@@ -1,8 +0,0 @@
-<?php
-require "vendor/autoload.php";
-use PhpOffice\PhpSpreadsheet\IOFactory;
-$spreadsheet = IOFactory::load("C:/Users/Advan/Downloads/INVENTARIS SARANA DAN PRASARANA DAN NON MEDIS.xlsx");
-$sheet = $spreadsheet->getSheetByName("Air Coundenser");
-$rows = $sheet->toArray();
-print_r(array_slice($rows, 0, 15));
-
